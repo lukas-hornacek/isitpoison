c7a96cc95bd7e3e1c51068db34aa6351e79c2433
lukas-hornacek <lukas.hornacek1928@gmail.com>
2025-02-20 19:34:03 +0100

    add database connection

---
+++ isitpoison-server/src/model/database_connection.ts
@@ -0,0 +1,13 @@
+import pg from 'pg'
+
+const { Client } = pg
+
+export const database = new Client({
+    user: 'isitpoisonuser',
+    password: 'password',
+    host: 'localhost',
+    port: 5432,
+    database: 'isitpoisondb',
+})
+
+await database.connect()
