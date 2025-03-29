855314ee5f1848fda677cc5280dd20e3e10d2495
lukas-hornacek <lukas.hornacek1928@gmail.com>
2025-03-29 14:57:48 +0100

    add admin only canteen endpoints

--- isitpoison-server/src/routers/canteen_router.ts
+++ isitpoison-server/src/routers/canteen_router.ts
@@ -1,5 +1,12 @@
 import express from "express";
-import { select_canteen, select_canteen_detail, select_canteens } from "../queries/canteen_queries.js";
+import { delete_canteen, insert_canteen, put_canteen, select_canteen, select_canteen_detail, select_canteens } from "../queries/canteen_queries.js";
+
+const hours = [
+    "monday_open", "monday_close", "tuesday_open", "tuesday_close",
+    "wednesday_open", "wednesday_close", "thursday_open", "thursday_close",
+    "friday_open", "friday_close", "saturday_open", "saturday_close",
+    "sunday_open", "sunday_close",
+];
 
 export const canteen_router = express.Router();
 
@@ -15,9 +22,10 @@ canteen_router.get("/:id(\\d+)", async (req, res) => {
     const result = await select_canteen(id);
 
     if (result.rowCount === 0) {
-        res.status(404).send("Canteen not found.");
+        res.status(404).send("canteen not found.");
+    } else {
+        res.json(result.rows[0]);
     }
-    res.json(result.rows[0]);
 });
 
 canteen_router.get("/detail/:id(\\d+)", async (req, res) => {
@@ -26,7 +34,113 @@ canteen_router.get("/detail/:id(\\d+)", async (req, res) => {
     const result = await select_canteen_detail(id);
 
     if (result.rowCount === 0) {
-        res.status(404).send("Canteen detail not found.");
+        res.status(404).send("canteen detail not found.");
+    } else {
+        res.json(result.rows[0]);
     }
-    res.json(result.rows[0]);
+});
+
+// TODO admin only
+canteen_router.delete("/:id(\\d+)", async (req, res) => {
+    const id = Number(req.params["id"]);
+    
+    try {
+        const result = await delete_canteen(id);
+        if (result.rowCount === 0) {
+            res.status(400).send("canteen does not exist");
+        } else {
+            res.send();
+        }
+    } catch (e: unknown) {
+        if (e instanceof Error) {
+            res.status(400).send(e.message);
+        } else {
+            res.status(500).send("unknown error");
+        }
+    }
+});
+
+// TODO admin only
+canteen_router.post("/", async (req, res) => {
+    const canteen = req.body;
+
+    if (typeof canteen["name"] !== "string") {
+        res.status(400).send("field 'name' is missing");
+        return;
+    }
+    if (typeof canteen["location"] !== "string") {
+        res.status(400).send("field 'location' is missing");
+        return;
+    }
+    for (const hour of hours) {
+        if (typeof canteen[hour] !== "string") {
+            res.status(400).send(`field '${hour}' is missing`);
+            return;
+        }
+        if (!/^([01][0-9]|2[0-3]):([0-5][0-9])$/.test(canteen[hour])) {
+            res.status(400).send(`invalid time format in field '${hour}'`);
+            return;
+        }
+    }
+
+    try {
+        await insert_canteen(canteen["name"], canteen["location"], canteen["monday_open"], canteen["monday_close"],
+            canteen["tuesday_open"], canteen["tuesday_close"], canteen["wednesday_open"], canteen["wednesday_close"],
+            canteen["thursday_open"], canteen["thursday_close"], canteen["friday_open"], canteen["friday_close"],
+            canteen["saturday_open"], canteen["saturday_close"], canteen["sunday_open"], canteen["sunday_close"]);
+        res.send();
+    } catch (e: unknown) {
+        if (e instanceof Error) {
+            if (e.message.includes("duplicate key")) {
+                res.status(400).send("canteen already exists.");
+            } else {
+                res.status(400).send(e.message);
+            }
+        } else {
+            res.status(500).send("unknown error.");
+        }
+    }
+});
+
+// TODO admin only
+canteen_router.put("/:id(\\d+)", async (req, res) => {
+    const id = Number(req.params["id"]);
+    const canteen = req.body;
+
+    if (typeof canteen["name"] !== "string") {
+        res.status(400).send("field 'name' is missing");
+        return;
+    }
+    if (typeof canteen["location"] !== "string") {
+        res.status(400).send("field 'location' is missing");
+        return;
+    }
+    for (const hour of hours) {
+        if (typeof canteen[hour] !== "string") {
+            res.status(400).send(`field '${hour}' is missing`);
+            return;
+        }
+        if (!/^([01][0-9]|2[0-3]):([0-5][0-9])$/.test(canteen[hour])) {
+            res.status(400).send(`invalid time format in field '${hour}'`);
+            return;
+        }
+    }
+
+    try {
+        const result = await put_canteen(id, canteen["name"], canteen["location"], canteen["monday_open"], canteen["monday_close"],
+            canteen["tuesday_open"], canteen["tuesday_close"], canteen["wednesday_open"], canteen["wednesday_close"],
+            canteen["thursday_open"], canteen["thursday_close"], canteen["friday_open"], canteen["friday_close"],
+            canteen["saturday_open"], canteen["saturday_close"], canteen["sunday_open"], canteen["sunday_close"]);
+        if (result.rowCount === 0) {
+            res.status(400).send("canteen does not exist");
+        } else {
+            res.send();
+        }
+    } catch (e: unknown) {
+        if (e instanceof Error) {
+            res.status(400).send(e.message);
+        } else {
+            res.status(500).send("unknown error.");
+        }
+    }
 });
