e973e7c53d01575a5e1f8a99da6df1428e28bc8d
lukas-hornacek <lukas.hornacek1928@gmail.com>
2025-03-30 14:13:37 +0200

    fix meal queries

--- isitpoison-server/src/queries/meal_queries.ts
+++ isitpoison-server/src/queries/meal_queries.ts
@@ -1,11 +1,12 @@
 import { database } from "../model/database_connection.js";
-import { Ordering, Weekday } from "../model/types.js";
+import { Ordering } from "../model/types.js";
 
 export async function select_meal(id: number) {
     const query = {
-        text: `SELECT meals.id, name, canteen_id, last_served, meals.uploaded, AVG(reviews.rating) AS rating
-        FROM meals, reviews
-        WHERE meals.id=$1 AND meals.id=reviews.meal_id
+        text: `SELECT meals.id, name, canteen_id, last_served, meals.uploaded, COALESCE(AVG(reviews.rating), 0) AS rating
+        FROM meals
+        LEFT OUTER JOIN reviews ON meals.id=reviews.meal_id
+        WHERE meals.id=$1
         GROUP BY meals.id, name, canteen_id, last_served, meals.uploaded`,
         values: [id],
     };
@@ -14,9 +15,11 @@ export async function select_meal(id: number) {
 
 export async function select_weekly_meals(canteen_id: number, weekday: number) {
     const query = {
-        text: `SELECT meals.id, name, canteen_id, last_served, meals.uploaded, AVG(reviews.rating) AS rating
-        FROM meals, reviews, weekly_meals
-        WHERE canteen_id=$1 AND weekday=$2 AND meals.id=reviews.meal_id AND meals.id=weekly_meals.meal_id
+        text: `SELECT meals.id, name, canteen_id, last_served, meals.uploaded, COALESCE(AVG(reviews.rating), 0) AS rating
+        FROM meals
+        JOIN weekly_meals ON meals.id=weekly_meals.meal_id
+        LEFT OUTER JOIN reviews ON meals.id=reviews.meal_id
+        WHERE canteen_id=$1 AND weekday=$2
         GROUP BY meals.id, name, canteen_id, last_served, meals.uploaded`,
         values: [canteen_id, weekday],
     };
@@ -24,9 +27,9 @@ export async function select_weekly_meals(canteen_id: number, weekday: number) {
 }
 
 export async function select_meals(canteen_ids: number[], ordering: Ordering, ascending: boolean, search?: string) {  
-    let text = `SELECT meals.id, name, canteen_id, last_served, meals.uploaded, AVG(reviews.rating) AS rating
-    FROM meals, reviews
-    WHERE meals.id=reviews.meal_id`;
+    let text = `SELECT meals.id, name, canteen_id, last_served, meals.uploaded, COALESCE(AVG(reviews.rating), 0) AS rating
+    FROM meals
+    LEFT OUTER JOIN reviews ON meals.id=reviews.meal_id`;
     const values = [];
 
     if (canteen_ids.length !== 0) {
@@ -79,29 +82,6 @@ export async function update_meal(id: number, name: string, canteen_id: number) 
     const query = {
         text: "UPDATE meals SET name=$1, canteen_id=$2 WHERE id=$3",
         values: [name, canteen_id, id],
-    };
-    return await database.query(query);
-}
-
-export async function update_last_served(id: number) {
-    const query = {
-        text: "UPDATE meals SET last_served=CURRENT_DATE WHERE id=$1",
-        values: [id],
-    };
-    return await database.query(query);
-}
-
-export async function delete_weekly_meals() {
-    const query = {
-        text: "DELETE FROM weekly_meals",
-    };
-    return await database.query(query);
-}
-
-export async function insert_weekly_meal(id: number, day: Weekday) {
-    const query = {
-        text: "INSERT INTO weekly_meals (meal_id, weekday) VALUES ($1, $2)",
-        values: [id, day]
     };
     return await database.query(query);
 }
