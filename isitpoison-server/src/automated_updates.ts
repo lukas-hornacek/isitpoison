610bfda8f2da28d97aa44de3beef382e64cc28cf
lukas-hornacek <lukas.hornacek1928@gmail.com>
2025-05-01 22:47:39 +0200

    improve automated updates

--- isitpoison-server/src/automated_updates.ts
+++ isitpoison-server/src/automated_updates.ts
@@ -1,6 +1,7 @@
 import * as cheerio from "cheerio";
 import { database } from "./model/database_connection.js";
 import { Weekday } from "./model/types.js";
+import { QueryResult, QueryResultRow } from "pg";
 
 enum Canteens {
     EatAndMeet = 1,
@@ -63,7 +64,7 @@ async function updateEatAndMeet() {
         const meals: string[] = $(`#day-${day} .desc`).map((_, el) => $(el).text().replace(/\(.*\)/g, "").trim()).get();
    
         for (const meal of meals) {
-            insert(meal, Canteens.EatAndMeet, (day % 7));
+            await insert(meal, Canteens.EatAndMeet, (day % 7));
         }
     }
 }
@@ -73,42 +74,41 @@ async function updateFunweston(canteen: Canteens) {
 
     const ids = ["fayn-food", "free-food", "fiit-food"];
 
-    $(`#${ids[canteen - 2]} .daily-offer > li`).each((i, dayElement) => {
-        $(dayElement)
-            .find(".day-offer > li")
-            .each((_, mealElement) => {
-                const mealText = $(mealElement).text().trim();
-                let end = mealText.search(/(\d+g)|(\d+(,\d+)?l)/);
-                if (end === -1) {
-                    end = mealText.search(/\d+(.\d+)?€/);
-                }
-                const meal = mealText.substring(3, end);
+    const dayElements = $(`#${ids[canteen - 2]} .daily-offer > li`).toArray();
+    let dayIndex = 0;
 
-                insert(meal, canteen, i + 1);
-            });
-    });
+    for (const dayElement of dayElements) {
+        const mealElements = $(dayElement).find(".day-offer > li").toArray();
+
+        for (const mealElement of mealElements) {
+            const mealText = $(mealElement).text().trim();
+            let end = mealText.search(/(\d+g)|(\d+(,\d+)?l)/);
+            if (end === -1) {
+                end = mealText.search(/\d+(.\d+)?€/);
+            }
+
+            const meal = mealText.substring(3, end);
+            await insert(meal, canteen, dayIndex + 1);
+        }
+        
+        dayIndex++;
+    }
 }
 
 async function insert(name: string, canteen_id: Canteens, day: Weekday) {
-    const select = {
-        text: "SELECT id FROM meals WHERE name=$1 AND canteen_id=$2",
-        values: [name, canteen_id],
-    };
-    let result = await database.query(select);
-
-    if (result.rowCount === 0) {
-        try {
-            const insert = {
-                text: "INSERT INTO meals (name, canteen_id, uploaded) VALUES ($1, $2, CURRENT_DATE) RETURNING id",
-                values: [name, canteen_id],
-            };
-            result = await database.query(insert);
-        } catch (e: unknown) {
-            if (e instanceof Error) {
-                console.log(e.message);
-            }
-            return;
-        }
+    let result: QueryResult<QueryResultRow>;
+    try {
+        const insert = {
+            text: "INSERT INTO meals (name, canteen_id, uploaded) VALUES ($1, $2, CURRENT_DATE) RETURNING id",
+            values: [name, canteen_id],
+        };
+        result = await database.query(insert);
+    } catch {
+        const select = {
+            text: "SELECT id FROM meals WHERE name=$1 AND canteen_id=$2",
+            values: [name, canteen_id],
+        };
+        result = await database.query(select);
     }
     
     const weekly = {
